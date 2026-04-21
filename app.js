2d122fefd989e2c04dcfc8431c572a90d685e04d
Pankaj Bhandari <94703661+bhandaripankaj@users.noreply.github.com>
2026-04-21 23:02:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express()
 connectDb()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
 app.use('/api',apiRouter)
 app.listen(3000,()=>{
     console.log("server is running.....")
